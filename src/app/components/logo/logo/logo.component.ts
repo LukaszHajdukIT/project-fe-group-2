5b736a840fd163667598adaf65dce625156cc9e8
One Visage <onevisage@interia.pl>
2020-12-13 11:42:59 +0100

    deleted not used things

--- src/app/components/logo/logo/logo.component.ts
+++ src/app/components/logo/logo/logo.component.ts
@@ -6,10 +6,4 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logo.component.scss']
 })
 export class LogoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
