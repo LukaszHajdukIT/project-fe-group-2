b11027570e9635e6605c91d86e72a4812776e432
One Visage <onevisage@interia.pl>
2020-12-20 09:29:22 +0100

    YASSS It is working

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class NavBarComponent {
-  links = [
+  links: ILink [] = [
     { path: 'home', title: 'Home' },
     { path: 'offer', title: 'Offer' },
   ];
+}
+
+interface ILink {
+ path: string;
+ title: string; 
 }
