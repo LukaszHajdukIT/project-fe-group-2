cfa800dd983f2688e508919c8dcc0091e1023698
One Visage <onevisage@interia.pl>
2020-12-20 14:55:05 +0100

    added http client but CROSS error

---
+++ src/app/services/inqoo-api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { CoursesResponse } from '../models/courses-response';
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InqooApiService {
+
+  constructor(private http: HttpClient) { }
+
+  private baseUrl = 'localhost:8080/courses';
+
+  getAllCourses(): Observable<CoursesResponse> {
+    return this.http.get<CoursesResponse>(this.baseUrl);
+  }
+}
