24c4e30a5bf0f9c3a2e9e7230bb6859450436a29
One Visage <onevisage@interia.pl>
2021-01-10 11:40:57 +0100

    category-details with params works

--- src/app/components/category-details/category-details.component.ts
+++ src/app/components/category-details/category-details.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-category-details',
   templateUrl: './category-details.component.html',
   styleUrls: ['./category-details.component.scss']
 })
-export class CategoryDetailsComponent{
+export class CategoryDetailsComponent implements OnInit{
+name: string;
 
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+      console.log(this.name);
+    });
+  }
 }
