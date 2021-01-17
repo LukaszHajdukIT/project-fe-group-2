70ed05131e5508ee0318817cad0d057f99eea14e
One Visage <onevisage@interia.pl>
2021-01-17 11:46:59 +0100

    params for courses not work

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CoursesResponse } from 'src/app/models/courses-response';
 import { InqooApiService } from 'src/app/services/inqoo-api.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-course-details',
@@ -10,12 +12,17 @@ import { InqooApiService } from 'src/app/services/inqoo-api.service';
 export class CourseDetailsComponent implements OnInit {
   courses: CoursesResponse[];
 
-  constructor(private service: InqooApiService) { }
+  constructor(
+    private inqooService: InqooApiService,
+    private route: ActivatedRoute
+    ) { }
   
-  ngOnInit(): void {
-    this.service.getAllCourses()
-      .subscribe(data => {
-        this.courses = data;
-      });
+  ngOnInit() {
+    this.route.queryParams.pipe(
+      switchMap(params => this.inqooService.getAllCoursesForSubcategory(params['id']))
+    ).subscribe(courses => {
+      this.courses = courses;
+      console.log('courses', courses)
+    });
   }
 }
