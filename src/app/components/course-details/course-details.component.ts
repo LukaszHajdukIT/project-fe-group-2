0a4fa10afc3838e1c80314e5334e745cfb3f640d
One Visage <onevisage@interia.pl>
2021-01-17 14:14:15 +0100

    Courses with subcategories works

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -1,28 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input } from '@angular/core';
 import { CoursesResponse } from 'src/app/models/courses-response';
-import { InqooApiService } from 'src/app/services/inqoo-api.service';
-import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrls: ['./course-details.component.scss']
 })
-export class CourseDetailsComponent implements OnInit {
-  courses: CoursesResponse[];
-
-  constructor(
-    private inqooService: InqooApiService,
-    private route: ActivatedRoute
-    ) { }
-  
-  ngOnInit() {
-    this.route.queryParams.pipe(
-      switchMap(params => this.inqooService.getAllCoursesForSubcategory(params['id']))
-    ).subscribe(courses => {
-      this.courses = courses;
-      console.log('courses', courses)
-    });
-  }
+export class CourseDetailsComponent {
+  @Input() course: CoursesResponse;
 }
--- src/app/components/subcategory-details/subcategory-details.component.ts
+++ src/app/components/subcategory-details/subcategory-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CoursesResponse } from 'src/app/models/courses-response';
 import { SubcategoriesResponse } from 'src/app/models/subcategories-response';
 import { InqooApiService } from 'src/app/services/inqoo-api.service';
 
@@ -11,10 +12,15 @@ export class SubcategoryDetailsComponent implements OnInit{
 
   @Input() subcategory: SubcategoriesResponse
 
+  courses: CoursesResponse[];
+
   constructor(private inqooService: InqooApiService) {
   }
 
   ngOnInit() {
     //TODO load courses for specific subcategory
+    this.inqooService.getAllCoursesForSubcategory(this.subcategory.id).subscribe(
+      data => this.courses = data
+    )
   }
 }
