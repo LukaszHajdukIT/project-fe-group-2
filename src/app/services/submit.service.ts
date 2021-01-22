ec474c52633190171fcf3249114d265ffc476f5b
One Visage <onevisage@interia.pl>
2021-01-22 21:12:42 +0100

    submit service

--- src/app/services/submit.service.ts
+++ src/app/services/submit.service.ts
@@ -4,13 +4,14 @@ import { Injectable } from '@angular/core';
 })
 export class SubmitService {
 
-  private checkedCourses: number[];
+  private checkedCourses: number[] = [];
 
   constructor() { }
 
   handleCourseCheck(courseId: number, isChecked: boolean){
       if(isChecked === true) this.checkedCourses.push(courseId);
       if(isChecked === false) this.checkedCourses = this.checkedCourses.filter( id => id !== courseId)
+      console.log(this.checkedCourses)
   }
 
   getCheckedCourses(): number[]{
